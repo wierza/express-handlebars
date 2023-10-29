6ca24db467ffd7a92077a3708990416e922a2f95
wierza <dawid@wierzycki.pl>
2023-10-29 19:27:23 +0100

    Add files

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { layout: 'dark' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+  })
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
